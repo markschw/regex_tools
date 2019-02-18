2e333d626479c9cee741bc602c875299750fe8ef
Mark Schwarzmann <31167893+markschw@users.noreply.github.com>
2019-02-18 19:04:12 +0200

    upload current work

---
+++ dfa_nfa.py
@@ -0,0 +1,184 @@
+# review: do i really need this?
+""" from itertools import chain, combinations
+def powerset(iterable):
+    orig_set = set(iterable)
+    return set(chain.from_iterable(combinations(orig_set, size)
+               for size in range(len(s)+1)))
+"""
+# todo: more extensive documentation
+
+
+class DFA:
+    def __init__(self, q0, delta, F):
+        self.q0 = q0
+        self.delta = delta
+        self.F = F
+
+    def accepts(self, word):
+        current_state = self.q0
+        for ch in word:
+            if (current_state, ch) not in self.delta:
+                return False
+            current_state = self.delta[current_state, ch]
+        return (current_state in self.F)
+
+    def rejects(self, word):
+        return not self.accepts(word)
+
+
+def test_DFA():
+    q0 = 0
+    F = {1}
+    delta = {(0, 'a'): 1, (1, 'a'): 0}
+    dfa = DFA(q0, delta, F)
+    assert dfa.rejects("")
+    assert dfa.accepts("a")
+    assert dfa.rejects("aa")
+    assert dfa.accepts("aaa")
+    assert dfa.rejects("b")
+    assert dfa.rejects("ab")
+
+if __name__ == "__main__":
+    print("Testing class DFA...")
+    test_DFA()
+    print("Test successful!")
+
+
+class NFA:  # supports epsilon transitions
+    def __init__(self, q0, delta, F):
+        self.q0 = q0
+        self.delta = delta
+        self.F = F
+
+    def Delta(self, states, ch):
+        """
+        An extention of delta to a set function on states.
+        """
+        result = set()
+        for q in states:
+            result.update(self.delta.get((q, ch), set()))
+        return result
+
+    def _epsilon_closure(self, states):
+        result = set()
+        new_states = states
+        while new_states:
+            result.update(new_states)
+            new_states = self.Delta(new_states, "") - result
+        return result
+
+    def accepts(self, word):
+        current_state_set = self._epsilon_closure({self.q0})
+        for ch in word:
+            current_state_set = self.Delta(current_state_set, ch)
+            current_state_set = self._epsilon_closure(current_state_set)
+        return bool(current_state_set & self.F)
+
+    def rejects(self, word):
+        return not self.accepts(word)
+
+
+def test_NFA():
+    q0 = 0
+    F = {2, 5}
+    delta = {(0, 'a'): {1, 3}, (0, 'b'): {7}, (1, 'b'): {2}, (2, 'c'): {6},
+             (3, 'a'): {5}, (3, 'b'): {4}, (5, 'a'): {4}}
+    nfa = NFA(q0, delta, F)
+    assert nfa.accepts("ab")
+    assert nfa.accepts("aa")
+    assert nfa.rejects("abc")
+    assert nfa.rejects("a")
+    assert nfa.rejects("b")
+    assert nfa.rejects("bbbbbb")
+    assert nfa.rejects("aab")
+    assert nfa.rejects("aaaaaaabc")
+    assert nfa.rejects("aaa")
+
+    q0 = 0
+    F = {3}
+    delta = {(0, ''): {1, 2}, (1, 'a'): {3}, (2, 'b'): {3}}
+    nfa = NFA(q0, delta, F)
+    assert nfa.accepts("a")
+    assert nfa.accepts("b")
+    assert nfa.rejects("ab")
+    assert nfa.rejects("aa")
+    assert nfa.rejects("bba")
+
+    q0 = 0
+    F = {3, 4}
+    delta = {(0, ''): {1, 2}, (1, 'a'): {1, 3}, (2, ''): {3}, (3, 'c'): {4}}
+    nfa = NFA(q0, delta, F)
+    assert nfa.accepts("a")
+    assert nfa.rejects("b")
+    assert nfa.rejects("ab")
+    assert nfa.accepts("aa")
+    assert nfa.rejects("bba")
+    assert nfa.accepts("c")
+    assert nfa.accepts("ac")
+    assert nfa.accepts("aac")
+    assert nfa.rejects("ca")
+
+if __name__ == "__main__":
+    print("Testing class NFA...")
+    test_NFA()
+    print("Test successful!")
+
+#note: All that follows is incomplete and untested.
+
+def NFA_to_DFA(nfa):
+    def NFA_to_eps_free_NFA(nfa):
+        # Create an initial state mimicking the epsilon closure of nfa.q0
+        new_q0 = nfa.q0
+        other_start_states = nfa._epsilon_closure({nfa.q0}) - {nfa.q0}
+        new_delta = {}
+        for (q, ch), R in nfa.delta.items():
+            if ch == "":
+                continue
+            if q in other_start_states:
+                q = new_q0
+            R = nfa._epsilon_closure(R)
+            if R & other_start_states:
+                R = (R - other_start_states) | {new_q0}
+            if (q, ch) not in new_delta:
+                new_delta[q, ch] = set()
+            new_delta[q, ch].update(R)
+        new_F = nfa.F
+        if new_F & other_start_states:
+            new_F = (new_F - other_start_states) | {new_q0}
+        return NFA(new_q0, new_delta, new_F)
+
+    # fixme:
+    def eps_free_NFA_to_DFA(nfa):
+        assert "" not in (ch for _, ch in nfa.delta())
+        new_q0 = set(nfa.q0)
+        new_delta = {}
+
+        transitions_left = nfa.delta()
+        # todo: replace with deque from collections lib
+        queue = [key for key in nfa.delta() if key[0] == nfa.q0]
+        current_q = nfa.q0
+        # while transitions_left:
+        #     for q in
+        #     (q, ch in nfa.delta() if state)
+
+
+#       new_delta = { (q_set, ch): set().union( *(self.delta.get((q, ch), {}))
+#                     for q in q_set) for q_set in powerset(Q) }
+#       new_F = powerset(q for (q, _) in nfa.delta())
+#        return NFA(new_q0, new_delta, new_F)
+
+    return eps_free_NFA_to_DFA(NFA_to_eps_free_NFA(nfa))
+
+
+# todo:
+def test_NFA_to_DFA():
+    q0 = 0
+    F = {3, 4}
+    delta = {(0, ''): {1, 2}, (1, 'a'): {1, 3}, (2, ''): {3}, (3, 'c'): {4}}
+    nfa = NFA(q0, delta, F)
+    print(NFA_to_DFA(nfa))
+
+if __name__ == "__main__":
+    print("Testing function NFA_to_DFA(nfa)...")
+    test_NFA_to_DFA()
+    print("Test successful!")
---
+++ regex_parser.py
@@ -0,0 +1,287 @@
+"""
+Some functions for working with regexes, including parsing and
+constructing matchers/recognizers (finite-state automata) for them.
+
+Formal grammar for (infix-form) regexes:
+R    -> Char | RR | R"|"R | R"*" | "("R")"
+Char -> a | b | ... | z | A | B | ... | Z | 0 | 1 | ... | 9
+In addition, whitespace may appear anywhere in the regex and
+has no semantic meaning.
+
+A regex in standardized form is defined the same, but has no whitespace
+whatsoever and has explicit concatenations, i.e. the rule R -> RR is
+replaced with R -> R"."R
+
+Formal grammar for Polish/prefix-form regexes:
+R    -> Char | "."RR | "|"RR | "*"R
+Char -> #as above - all alphanumerics
+"""
+
+# todo: handle invalid inputs to functions
+# todo: go over documentation
+
+from dfa_nfa import NFA
+
+
+def standardize(regex):
+    """
+    Returns a modified regex with all the whitespace removed and
+    with explicit concatenations (by inserting "." where necessary).
+    """
+    regex = [ch for ch in regex if not ch.isspace()]
+    result = []
+    for i in range(len(regex)):
+        result.append(regex[i])
+        if i == len(regex) - 1:
+            break
+        pattern = "".join("A" if ch.isalnum() else ch
+                          for ch in regex[i:i + 2])
+        concatenation_patterns = {"AA", "A(", "*A", "*(", ")(", ")A"}
+        if pattern in concatenation_patterns:
+            result.append(".")
+    return "".join(result)
+
+
+def test_stanardize():
+    assert standardize(" a |b c") == "a|b.c"
+    assert standardize("ab| c ") == "a.b|c"
+    assert standardize(" ( a * b c * ) *") == "(a*.b.c*)*"
+    assert standardize("d | (a*b |c *)e") == "d|(a*.b|c*).e"
+
+if __name__ == "__main__":
+    print("Testing function standardize(regex)...")
+    test_stanardize()
+    print("Test successful!")
+
+
+def infix_to_prefix(regex):
+    """
+    Returns a whitespace-free regex in prefix-form which is
+    semantically equivalent to the given regex.
+    """
+    # A version of Dijkstra's shunting-yard algorithm
+    regex = standardize(regex)
+    op_stack = []
+    result = []
+    for ch in reversed(regex):
+        if ch.isspace():
+            continue
+        if ch.isalnum():
+            result.append(ch)
+        elif ch == "*":
+            op_stack.append(ch)
+        elif ch == ".":
+            while op_stack and op_stack[-1] == "*":
+                result.append("*")
+                op_stack.pop()
+            op_stack.append(ch)
+        elif ch == "|":
+            while op_stack and op_stack[-1] in ".*":
+                result.append(op_stack[-1])
+                op_stack.pop()
+            op_stack.append(ch)
+        elif ch == ")":
+            op_stack.append(ch)
+        elif ch == "(":
+            assert op_stack
+            while op_stack[-1] != ")":
+                result.append(op_stack[-1])
+                op_stack.pop()
+            op_stack.pop()  # pop the ")"
+            if op_stack:
+                result.append(op_stack[-1])
+                op_stack.pop()
+        else:
+            raise ValueError("Given regex contains an invalid character.")
+    result.extend(reversed(op_stack))
+    result.reverse()
+    return "".join(result)
+
+
+def test_infix_to_prefix():
+    assert infix_to_prefix("a|b.c") == "|a.bc"
+    assert infix_to_prefix("a.b|c") == "|.abc"
+    assert infix_to_prefix("(a*.b.c*)*") == "*..*ab*c"
+    assert infix_to_prefix("d|(a*.b|c*).e") == "|d.|.*ab*ce"
+
+if __name__ == "__main__":
+    print("Testing function infix_to_frefix(regex)...")
+    test_infix_to_prefix()
+    print("Test successful!")
+
+
+class Node:
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return "Node('{}', {}, {})".format(self.data, self.left, self.right)
+
+
+def construct_parse_tree(regex):
+    """
+    Returns a parse tree for a given (infix-form) regex.
+    """
+    regex = infix_to_prefix(standardize(regex))
+
+    def construct_branch(tail):
+        """
+        Given the suffix of a whitespace-free regex in prefix form,
+        this function returns a parse tree for the maximal prefix of
+        the input that forms a valid regex, along with the remainder
+        of the regex. Thus the return value is a pair (tree, remainder).
+
+        Example: construct_branch(".ab*c") should return the parse tree
+                 for ".ab" along with the string "*c".
+        """
+        if tail == "":
+            return None, ""
+        ch = tail[0]
+        if ch.isalnum():
+            return Node(ch, None, None), tail[1:]
+        elif ch == "*":
+            left, remainder = construct_branch(tail[1:])
+            return Node(ch, left, None), remainder
+        elif ch in ".|":
+            left, remainder = construct_branch(tail[1:])
+            right, remainder = construct_branch(remainder)
+            return Node(ch, left, right), remainder
+        else:
+            raise ValueError("Given input is not a suffix of a valid regex.")
+
+    tree, _ = construct_branch(regex)
+    return tree
+
+
+def test_construct_parse_tree():
+    regex = "d|(a*b|c*)e"
+    print("The following should be a parse tree for " + regex + "...")
+    tree = construct_parse_tree(regex)
+    print(tree)
+    print("Parse tree printed!")
+
+if __name__ == "__main__":
+    test_construct_parse_tree()
+
+
+def to_DOT_format(tree):
+    """
+    Returns a string in the DOT format which represents the given tree.
+    """
+    def format_branch(tree):
+        if tree is None:
+            return ""
+        # todo: use a counter to label the states instead of node ids.
+        sons = format_branch(tree.left) + format_branch(tree.right)
+        father = str(id(tree)) + ' [label="' + str(tree.data) + '"];\n'
+        edges = ""
+        for node in [tree.left, tree.right]:
+            if node is not None:
+                edges += str(id(tree)) + " -> " + str(id(node)) + ";\n"
+        return sons + father + edges
+
+    return "digraph { \n" + format_branch(tree) + "}\n"
+
+
+def test_to_DOT_format():
+    regex = "d|(a*b|c*)e"
+    tree = construct_parse_tree(regex)
+    print("The following should be a representation of the parse tree of",
+          regex, "in the DOT format...")
+    print(to_DOT_format(tree))
+    print("DOT-format tree printed!")
+
+if __name__ == "__main__":
+    test_to_DOT_format()
+
+
+def construct_matcher(regex):
+    state_counter = 0
+
+    def construct_NFA(tree):
+        nonlocal state_counter
+        assert tree
+        token = tree.data
+        if token.isalnum():
+            assert(not tree.left and not tree.right)
+            q0 = state_counter
+            q1 = state_counter + 1
+            state_counter += 2
+            delta = {(q0, token): {q1}}
+            F = {q1}
+        elif token == "*":
+            assert(tree.left and not tree.right)
+            son_nfa = construct_NFA(tree.left)
+            q0 = son_nfa.q0
+            delta = son_nfa.delta
+            for q in son_nfa.F:
+                if (q, "") not in delta:
+                    delta[q, ""] = set()
+                delta[q, ""].add(q0)
+            F = son_nfa.F.union({q0})
+        elif token == ".":
+            assert(tree.left and tree.right)
+            left_nfa = construct_NFA(tree.left)
+            right_nfa = construct_NFA(tree.right)
+            q0 = left_nfa.q0
+            delta = {**left_nfa.delta, **right_nfa.delta}
+            for q in left_nfa.F:
+                if (q, "") not in delta:
+                    delta[q, ""] = set()
+                delta[q, ""].add(right_nfa.q0)
+            F = right_nfa.F
+        elif token == "|":
+            assert(tree.left and tree.right)
+            left_nfa = construct_NFA(tree.left)
+            right_nfa = construct_NFA(tree.right)
+            q0 = state_counter
+            state_counter += 1
+            delta = {**left_nfa.delta, **right_nfa.delta}
+            delta[q0, ""] = {left_nfa.q0, right_nfa.q0}
+            F = left_nfa.F.union(right_nfa.F)
+        else:
+            assert 0, "Invalid token found in tree: " + token
+        return NFA(q0, delta, F)
+
+    return construct_NFA(construct_parse_tree(regex))
+
+
+# todo: a more comprehensive test
+def test_construct_matcher():
+    matcher = construct_matcher("d|(a*b|c*)e")
+    # review:
+    # print(matcher.delta)
+    assert matcher.accepts("d")
+    assert matcher.accepts("e")
+    assert matcher.accepts("ce")
+    assert matcher.accepts("ccce")
+    assert matcher.accepts("be")
+    assert matcher.accepts("abe")
+    assert matcher.accepts("aabe")
+    assert matcher.rejects("")
+    assert matcher.rejects("da")
+    assert matcher.rejects("ec")
+    assert matcher.rejects("de")
+    assert matcher.rejects("b")
+    assert matcher.rejects("a")
+    assert matcher.rejects("ae")
+    assert matcher.rejects("ab")
+    assert matcher.rejects("ace")
+    assert matcher.rejects("abce")
+    assert matcher.rejects("dabe")
+
+    # review:
+    matcher2 = construct_matcher("a*b*")
+    assert matcher2.accepts("")
+    assert matcher2.accepts("a")
+    assert matcher2.accepts("b")
+    assert matcher2.accepts("ab")
+    assert matcher2.accepts("aabb")
+    assert matcher2.rejects("ba")
+
+if __name__ == "__main__":
+    print("Testing function construct_matcher(regex)...")
+    test_construct_matcher()
+    print("Test successful!")
